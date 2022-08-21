e299022a12325e1df787d6f108b8ec40982c3773
Joseph Dillon <joseph.dillon.522@gmail.com>
2022-08-20 22:30:59 -0400

    squared away the cookies to headers change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from './shared/cart-service/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cart-ui-test';
+export class AppComponent implements OnInit {
+  public cartCount$: Observable<number> = this.cartService.cartCount$;
+
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  ngOnInit(): void {
+    this.cartService.getCart();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PaymentModule } from './payment/payment.module';
+import { CardModule } from './shared/card/card.module';
+import { CartServiceModule } from './shared/cart-service/cart-service.module';
+import { CartIdInterceptor } from './shared/interceptors/cartId';
 
 const routes: Route[] = [
   {
-    path: '',
+    path: 'catalog',
+    loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule)
+  },
+  {
+    path: 'checkout',
     loadChildren: () => import('./payment/payment.module').then(m => m.PaymentModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'catalog'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'catalog'
   }
 ]
 
@@ -18,10 +41,14 @@ const routes: Route[] = [
   ],
   imports: [
     BrowserModule,
-    PaymentModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    CardModule,
+    CartServiceModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: CartIdInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
