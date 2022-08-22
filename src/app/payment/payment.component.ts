fe7d88c1af7c97b0375858128dc340a3a919f303
Joseph Dillon <joseph.dillon.522@gmail.com>
2022-08-22 17:43:03 -0400

    fiddling with form submissions

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FundsAvailableValidator } from '../shared/validators/fundsAvailable.validator';
 import { PaymentService } from './payment.service';
@@ -6,6 +6,7 @@ import { mergeWith, takeUntil, tap } from 'rxjs/operators';
 import { firstValueFrom, Subject } from 'rxjs';
 import { IPaymentForm, IPaymentFormValue } from '../shared/interfaces/payment';
 import { CartService } from '../shared/cart-service/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -31,23 +32,36 @@ export class PaymentComponent implements OnInit, OnDestroy{
     })
   });
 
+  get paymentAmount(): FormControl<number> {
+    return this.paymentForm.get('paymentAmount') as FormControl<number>;
+  }
+
   // The only manual subscription because its getting late...
   private destroyed$ = new Subject();
 
   constructor(
     private paymentService: PaymentService,
     private fundsAvailable: FundsAvailableValidator,
-    private cartService: CartService
+    private cartService: CartService,
+    private changeDetector: ChangeDetectorRef,
+    private zone: NgZone
   ) { }
 
   ngOnInit(): void {
     this.paymentService.getAvailableBalance();
     this.cartService.cart$.pipe(takeUntil(this.destroyed$)).subscribe(cart => {
-      this.paymentForm.patchValue({ paymentAmount: cart.totalCost });
-      this.paymentForm.get('paymentAmount')?.markAsTouched();
-      this.paymentForm.get('paymentAmount')?.updateValueAndValidity();
-      this.paymentForm.get('paymentAmount')?.disable();
+      this.paymentAmount.setValue(cart.totalCost);
+      this.paymentAmount.markAsTouched();
+      this.paymentAmount.updateValueAndValidity();
     });
+    /**
+     * This is apparently a known bug with a fix out there but I dont know if its been released
+     * https://github.com/angular/angular/issues/22556
+     */
+    this.changeDetector.detectChanges();
+    setTimeout(() => {
+      this.paymentAmount.disable();
+    }, 0);
   }
 
   ngOnDestroy(): void {
@@ -70,8 +84,8 @@ export class PaymentComponent implements OnInit, OnDestroy{
 
   public async addToBalance(amount: number) {
     await firstValueFrom(this.paymentService.addToBalance(amount));
-    this.paymentForm.get('paymentAmount')?.markAsTouched();
-    this.paymentForm.get('paymentAmount')?.updateValueAndValidity();
+    this.paymentAmount.markAsTouched();
+    this.paymentAmount.updateValueAndValidity();
   }
 
   public reset(): void {
